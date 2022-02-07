deaf3235552d72971a63dfcdb2acbec395d619c3
Kerrigan <kavex1988@gmail.com>
2022-02-07 10:44:30 -0700

    Update WindowsUpgradeSilentInstaller.ps1

--- PowerShell/WindowsUpgradeSilentInstaller.ps1
+++ PowerShell/WindowsUpgradeSilentInstaller.ps1
@@ -1,7 +1,12 @@
 $dir = 'C:\Temp\WUA\'
 mkdir $dir
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path C:\temp\WUA\console.txt -append
 $webClient = New-Object System.Net.WebClient
 $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "$($dir)\Win10Upgrade.exe"
 $webClient.DownloadFile($url,$file)
 Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
+Stop-Transcript
