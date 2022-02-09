d115027bee90633787f6cdd008b8835ad19660ef
Kerrigan <kavex1988@gmail.com>
2022-02-09 14:14:20 -0700

    Notes Updates

--- PowerShell/WindowsUpgradeSilentInstaller.ps1
+++ PowerShell/WindowsUpgradeSilentInstaller.ps1
@@ -9,4 +9,8 @@ $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "$($dir)\Win10Upgrade.exe"
 $webClient.DownloadFile($url,$file)
 Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /copylogs $dir'
+[datetime]$RestartTime = '9PM'
+[datetime]$CurrentTime = Get-Date
+[int]$WaitSeconds = ( $RestartTime - $CurrentTime ).TotalSeconds
+shutdown -r -t $WaitSeconds
 Stop-Transcript
