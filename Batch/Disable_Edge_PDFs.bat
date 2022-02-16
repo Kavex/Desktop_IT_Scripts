2ab462d68e938dc2c4fac1b29ed6c83d0d73b125
Kerrigan <kavex1988@gmail.com>
2022-02-16 13:16:19 -0700

    Update Disable_Edge_PDFs.bat

--- Batch/Disable_Edge_PDFs.bat
+++ Batch/Disable_Edge_PDFs.bat
@@ -1,4 +1,4 @@
-REM disables open with app and defaults for edge 
+REM disables open with app and defaults for edge for html and pdfs
 REM https://eddiejackson.net/wp/?p=16195
 REM https://www.petenetlive.com/KB/Article/0001395
 REG ADD "HKEY_CURRENT_USER\Software\Classes\AppX4hxtad77fbk3jkkeerkrm0ze94wjf3s9" /v NoStaticDefaultVerb /t REG_SZ /f
