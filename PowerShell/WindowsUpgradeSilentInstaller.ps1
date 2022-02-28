c010aa6e14a447bcf9cce073921a14c49c46df34
Kerrigan <kavex1988@gmail.com>
2022-02-28 15:52:44 -0700

    Added SFTA and remove transcribe from silent installer

--- PowerShell/WindowsUpgradeSilentInstaller.ps1
+++ PowerShell/WindowsUpgradeSilentInstaller.ps1
@@ -1,9 +1,5 @@
 $dir = 'C:\Temp\WUA\'
 mkdir $dir
-$ErrorActionPreference="SilentlyContinue"
-Stop-Transcript | out-null
-$ErrorActionPreference = "Continue"
-Start-Transcript -path C:\temp\WUA\console.txt -append
 $webClient = New-Object System.Net.WebClient
 $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
 $file = "$($dir)\Win10Upgrade.exe"
@@ -13,4 +9,3 @@ Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgra
 [datetime]$CurrentTime = Get-Date
 [int]$WaitSeconds = ( $RestartTime - $CurrentTime ).TotalSeconds
 shutdown -r -t $WaitSeconds
-Stop-Transcript
