06820e2cab325d0ac05b0f050ae5bb2a3cc48d7a
Kerrigan <kavex1988@gmail.com>
2022-06-21 15:40:49 -0600

    Check for windows updates

---
+++ PowerShell/Check_Windows_Updates_AutoReboot.ps1
@@ -0,0 +1,11 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Start-Sleep -Seconds 30
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Start-Sleep -Seconds 30
+Set-PSRepository -Name "psgallery" -InstallationPolicy Trusted
+Start-Sleep -Seconds 30
+Install-Module PSWindowsUpdate -force
+Start-Sleep -Seconds 30
+Get-WindowsUpdate
+Start-Sleep -Seconds 300
+Install-WindowsUpdate -AcceptAll -Install -AutoReboot | Format-Table -Wrap | Out-File "C:\temp\$(get-date -f yyyy-MM-dd)-WindowsUpdate.log" -Append -force
