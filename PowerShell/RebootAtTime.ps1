23c26f25a722fb3b84489f4dc7f3e5824bb09122
Kerrigan <kavex1988@gmail.com>
2022-08-04 13:37:30 -0600

    Reboot and CLI Tool

--- PowerShell/RebootAtTime.ps1
+++ PowerShell/RebootAtTime.ps1
@@ -1,4 +1,4 @@
-[datetime]$RestartTime = '9PM'
+[datetime]$RestartTime = '10PM'
 [datetime]$CurrentTime = Get-Date
 [int]$WaitSeconds = ( $RestartTime - $CurrentTime ).TotalSeconds
 shutdown -r -t $WaitSeconds
